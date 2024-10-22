844f0e9a8a02d80bf7ceac1abeebfc77e089fdb8
Shahzad Khan <shahzad@smash.cloud>
2024-10-22 23:45:04 +0500

    build 14

--- cypress/e2e/Thermo/Estimator/Estimator-Molded.cy.js
+++ cypress/e2e/Thermo/Estimator/Estimator-Molded.cy.js
@@ -100,12 +100,12 @@ describe('Estimator Order Creation/Edition/Submission For Molded Category Suit',
         cy.get('.MuiAlert-message').contains('Please validate the form before submitting the order').should('be.visible')                                       // Assertion for validation message
         
         cy.get('.MuiAccordionSummary-root').click().wait(500)                                                                                                   // Tapping to Accordian 
-        
+        cy.get('.jss23 > .MuiButtonBase-root').click().wait(500)
         //cy.get('[id=":rt:-helper-text"]').should('have.text', 'Required value').should('be.visible')                                                             // Order Name Validation of helper text
         //cy.get('[id=":r11:-helper-text"]').contains('Value is not a valid email address').should('be.visible')                                                    // Eamil Validation of helper text
         //cy.get('[id=":r17:-helper-text"]').contains('Required value').should('be.visible')                                                                        // PO Validation of helper text 
         //cy.wait(10000)
-        cy.get('[id=":rt:"]', { timeout: 10000 }).should('exist');
+        cy.get('[id=":rt:"]', { timeout: 10000 }).should('exist')
         cy.get('[id=":rt:"]').type('Automate Doors Category')                                                                                                     // Name         
         cy.get('[id=":r11:"]').type('AutomateDoors#gmail.com', { force: true })                                                                                   // Email
         cy.get('[id=":r11:-helper-text"]').contains('Value is not a valid email address').should('be.visible')                                                    // Eamil Validation of helper text        
