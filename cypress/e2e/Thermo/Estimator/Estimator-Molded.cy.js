f3fc4c9a7c01e324817f2b27cdb458fd9203cef4
Shahzad Khan <shahzad@smash.cloud>
2024-10-23 19:59:55 +0500

    build 8

--- cypress/e2e/Thermo/Estimator/Estimator-Molded.cy.js
+++ cypress/e2e/Thermo/Estimator/Estimator-Molded.cy.js
@@ -333,12 +333,12 @@ describe('Estimator Order Creation/Edition/Submission For Molded Category Suit',
         cy.get('#\\:r7v\\:').should('contain', 'B')                                                                                                             // Assertion of postiton            
         cy.get(':nth-child(3) > :nth-child(15) > .MuiFormControl-root > .MuiButtonBase-root > .PrivateSwitchBase-input').should('be.checked')                   // Assertion of Dowels                     
         cy.get('#\\:r80\\:').should('have.value', '61 - Antique White Silk')                                                                                    // Assertion of color                
-        cy.get('[aria-label="Save"] > .MuiButtonBase-root').click().wait(5000)                                                                                  // Tapping to save btn
-        cy.get('.headerActions > .MuiButton-contained').click().wait(10000)                                                                                     // Tapping to Print btn
-        cy.get('#\\:rq\\: > .MuiButtonBase-root').click()                                                                                                       // Closing print view
+        //cy.get('[aria-label="Save"] > .MuiButtonBase-root').click().wait(5000)                                                                                  // Tapping to save btn
+        //cy.get('.headerActions > .MuiButton-contained').click().wait(10000)                                                                                     // Tapping to Print btn
+        //cy.get('#\\:rq\\: > .MuiButtonBase-root').click()                                                                                                       // Closing print view
         cy.get('.css-9hpeq4').contains('Submit').should('be.enabled').click().wait(5000)                                                                        // Tapping to Submit btn
-        //cy.get('.css-1ubxkj1 > :nth-child(1)').click().wait(5000)                                                                                               // Print order    
-        //cy.get('#\\:rq\\: > .MuiButtonBase-root').click().wait(500)                                                                                             // Closing print view
+        cy.get('.css-1ubxkj1 > :nth-child(1)').click().wait(5000)                                                                                               // Print order    
+        cy.get('#\\:rq\\: > .MuiButtonBase-root').click().wait(500)                                                                                             // Closing print view
         
 
 
