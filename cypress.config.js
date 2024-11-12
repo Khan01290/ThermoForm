9b7ac49cefcc3c81035c4f7c2d901d75f2ceefa4
Shahzad Khan <shahzad@smash.cloud>
2024-11-13 02:56:24 +0500

    build 1

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 
 module.exports = defineConfig({
-  projectId: "tp8fdu",
+  projectId: "dijb3w",
   pageLoadTimeout: 120000, // Set the page load timeout to 120 seconds (2 minutes)
   reporter: 'cypress-mochawesome-reporter',   // For HTML Reporter
   e2e: {
